5353d505e4c4b9d8e6e8f43e6e375b726b1de67b
SUKHNAM GILL <sukhnam23@gmail.com>
2024-10-15 20:25:12 +0530

    Update hello_world_1.py

--- Graphical User Interface/hello_world_1.py
+++ Graphical User Interface/hello_world_1.py
@@ -1,1 +1,18 @@
-print("hello world In gui" )
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+def window():
+    app=QApplication(sys.argv)
+    w=QWidget()
+    w.setGeometry(100,100,250,50)
+    b=QLabel(w)
+    b.setText("Hello sukhnam")
+    b.move(50,20)
+    w.setWindowTitle("hello_world_1")
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ =='__main__':
+    window()
+
